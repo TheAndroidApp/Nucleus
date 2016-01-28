e4ded3e3a6d526d01a285ccf4e8eccaedbeb52a1
Anand-M-P <mailanandmp@gmail.com>
2016-01-29 00:40:30 +0530

    Floating Action Button added. New layout fab_activity added to the layout folder.

---
+++ app/src/main/java/com/nucleus/app/SplashActivity.java
@@ -0,0 +1,42 @@
+package com.nucleus.app;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+
+
+public class SplashActivity extends AppCompatActivity {
+
+    //Splash Timer
+    private static int SPLASH_TIME_OUT = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        //        For creating a full-screen activity. Since we're extending AppCompat library add request feauture() here.
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
+}
