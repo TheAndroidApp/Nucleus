c6b790483d719c1b35804cc8cce93f377b1c1745
Sreeraag Mohan <sreeraagmohan@gmail.com>
2016-02-02 14:51:36 +0530

    Fixed most stuff. Theme is up and ready - yet to be perfected though. Toolbar text is now bold. FAB icons have to made a shade lighter - will do in next commit.

--- app/src/main/java/com/nucleus/app/MainActivity.java
+++ app/src/main/java/com/nucleus/app/MainActivity.java
@@ -1,17 +1,13 @@
 package com.nucleus.app;
 
 import android.content.Intent;
-//import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.NavigationView;
-//import android.support.design.widget.Snackbar;
+import android.support.design.widget.NavigationView;;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
@@ -32,11 +28,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fab_activity);
+        setContentView(R.layout.activity_main);
 
 
         ImageView icon = new ImageView(this); // Create an icon
-        icon.setImageDrawable(getDrawable(R.drawable.ic_menu_black_24dp));
+        icon.setImageDrawable(getDrawable(R.drawable.ic_add_black_24dp));
 
         FloatingActionButton actionButton = new FloatingActionButton.Builder(this)
                 .setContentView(icon)
@@ -90,66 +86,72 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        /* Navigation Drawer Functions Start Here. Please do not change! */
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
-        mNavigationView = (NavigationView) findViewById(R.id.shitstuff);
+        mNavigationView = (NavigationView) findViewById(R.id.nav_view);
 
-
+        /* Tab Fragments Initialized Here */
         mFragmentManager = getSupportFragmentManager();
-        mFragmentTransaction = mFragmentManager.beginTransaction();
-        mFragmentTransaction.replace(R.id.containerView, new TabFragment()).commit();
-
+        mFragmentTransaction= mFragmentManager.beginTransaction();
+        mFragmentTransaction.replace(R.id.containerView,new TabFragment()).commit();
 
         mNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+
             @Override
             public boolean onNavigationItemSelected(MenuItem menuItem) {
                 mDrawerLayout.closeDrawers();
 
+                //Checking if the item is in checked state or not, if not make it in checked state
+                if (menuItem.isChecked()) menuItem.setChecked(false);
+                else menuItem.setChecked(true);
 
-                if (menuItem.getItemId() == R.id.nav_item_sent) {
-                    FragmentTransaction fragmentTransaction = mFragmentManager.beginTransaction();
-                    fragmentTransaction.replace(R.id.containerView, new SentFragment()).commit();
+                //Closing drawer on item click
+                mDrawerLayout.closeDrawers();
 
+                //Check to see which item was being clicked and perform appropriate action
+                switch (menuItem.getItemId()) {
+
+
+                    case R.id.stats:
+                        Toast.makeText(getApplicationContext(), "Stats Selected", Toast.LENGTH_SHORT).show();
+                        return true;
+
+                    // For rest of the options we just show a toast on click
+
+                    case R.id.settings:
+                        Toast.makeText(getApplicationContext(), "Settings Selected", Toast.LENGTH_SHORT).show();
+                        return true;
+                    case R.id.about:
+                        Toast.makeText(getApplicationContext(), "About Selected", Toast.LENGTH_SHORT).show();
+                        return true;
+                    default:
+                        Toast.makeText(getApplicationContext(), "Something is Wrong", Toast.LENGTH_SHORT).show();
+                        return true;
                 }
-
-                if (menuItem.getItemId() == R.id.nav_item_inbox) {
-                    FragmentTransaction xfragmentTransaction = mFragmentManager.beginTransaction();
-                    xfragmentTransaction.replace(R.id.containerView, new TabFragment()).commit();
-                }
-
-                return false;
             }
-
         });
 
 
         android.support.v7.widget.Toolbar toolbar = (android.support.v7.widget.Toolbar) findViewById(R.id.toolbar);
-        ActionBarDrawerToggle mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, toolbar, R.string.app_name,
-                R.string.app_name);
+        ActionBarDrawerToggle mDrawerToggle = new ActionBarDrawerToggle(this,mDrawerLayout,toolbar,R.string.openDrawer, R.string.closeDrawer){
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                // Code here will be triggered once the drawer closes as we don't want anything to happen so we leave this blank
+                super.onDrawerClosed(drawerView);
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                // Code here will be triggered once the drawer open as we don't want anything to happen so we leave this blank
+
+                super.onDrawerOpened(drawerView);
+            }
+        };
 
         mDrawerLayout.setDrawerListener(mDrawerToggle);
-
         mDrawerToggle.syncState();
 
     }
-
-//    @Override
-//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-//
-//        super.onActivityResult(requestCode, resultCode, data);
-//        if (resultCode == RESULT_OK){
-//            Bundle extras = data.getExtras();
-//            bmp = (Bitmap) extras.get("data");
-//            iv.setImageBitmap(bmp);
-//
-//
-//            Bundle basket = new Bundle();
-//            Intent a = new Intent(Data.this, OpenedClass.class);
-//            a.putExtras(basket);
-//            startActivity(a);
-//
-//
-//        }
-//    }
-
 }
 
